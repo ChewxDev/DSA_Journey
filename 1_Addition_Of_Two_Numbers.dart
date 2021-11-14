e7944fc9c0d3c4526fdee61e98d399912df19b5d
ChewxDev <91054021+ChewxDev@users.noreply.github.com>
2021-11-14 20:04:58 +0100

    Update 1_Addition_Of_Two_Numbers.dart

--- 1_Addition_Of_Two_Numbers.dart
+++ 1_Addition_Of_Two_Numbers.dart
@@ -1,18 +1,18 @@
 import "dart:io";
 
 void main(){
-  List<int> arr = [];
-  arr.addAll([int.parse(stdin.readLineSync()), int.parse(stdin.readLineSync())]);
-  print(arr[0] + arr[1]);
+  List<int> arr = []; //Initializing the empty List
+  arr.addAll([int.parse(stdin.readLineSync()), int.parse(stdin.readLineSync())]); //Telling the useer to input two integers from the keyboard
+  print(arr[0] + arr[1]); //Adding the two inputs
 }
 
 
 
-int sumOfTwoDigits(int a, int b){
-  return a+b;
+int sumOfTwoDigits(int a, int b){ //Using a function with two parameters 
+  return a+b; //Adding the two parameters
 }
 
 void main(){
-  int result = sumOfTwoDigits(int.parse(stdin.readLineSync()), int.parse(stdin.readLineSync()));
-  print(result);
+  int result = sumOfTwoDigits(int.parse(stdin.readLineSync()), int.parse(stdin.readLineSync())); //Calling the functions and adding arguments from the keyboard
+  print(result); // printing the result
 }
