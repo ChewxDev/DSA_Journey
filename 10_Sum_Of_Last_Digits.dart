b526146564778982411fe93f15787dd31dfa2e4c
ChewxDev <91054021+ChewxDev@users.noreply.github.com>
2021-11-17 07:49:19 +0100

    Update 10_Sum_Of_Last_Digits.dart

--- 10_Sum_Of_Last_Digits.dart
+++ 10_Sum_Of_Last_Digits.dart
@@ -26,3 +26,47 @@ void main(){
   num answer = fib(num.parse(stdin.readLineSync()));
   print(answer);
 }
+
+//Fast Solution
+import "dart:io";
+
+List<int> pisanno(int a){
+  int sum = 0;
+  List<int> fib_arr = [0,1];
+  for(int i = 1; i<=a*a; i++){
+    int value = fib_arr[0] + fib_arr[1];
+    fib_arr[0] = fib_arr[1];
+    fib_arr[1] = value % a;
+    sum += value % a;
+    if(fib_arr[0] == 0 && fib_arr[1] == 1){
+      return [i, sum];
+    }
+  }
+}
+
+int fib(int n){
+  List<int> period = pisanno(10);
+  //print(period);
+  int m = n~/period[0];
+  int sum = period[1] * m;
+   
+  n = n%period[0];
+  //print(n);
+  if (n){
+    sum += 1;
+  }
+  List<int> fib_arr = [0,1];
+  for(int i = 2; i<=n; i++){
+    int value = (fib_arr[0] + fib_arr[1]) % 10;
+    fib_arr[0] = fib_arr[1];
+    fib_arr[1] = value;
+    sum += value;
+    }
+//print(fib_arr);
+return sum % 10;
+}
+
+void main(){
+  num answer = fib(num.parse(stdin.readLineSync()));
+  print(answer);
+}
