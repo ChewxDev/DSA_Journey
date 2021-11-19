54d3c1c4d858539eef372b150886316645c6b89e
ChewxDev <91054021+ChewxDev@users.noreply.github.com>
2021-11-19 02:32:25 +0100

    Create 12_Last_Digit_of_the_Sum_of_Squares_of_Fibonacci_Numbers.dart

---
+++ 12_Last_Digit_of_the_Sum_of_Squares_of_Fibonacci_Numbers.dart
@@ -0,0 +1,45 @@
+import "dart:io";
+import "dart:math";
+
+List<int> pisanno(int a){
+  int sum = 0;
+  List<int> fib_arr = [0,1];
+  for(int i = 1; i<=a*a; i++){
+    int value = fib_arr[0] + fib_arr[1];
+    fib_arr[0] = fib_arr[1];
+    fib_arr[1] = value % a;
+    sum += pow(value % a,2);
+    if(fib_arr[0] == 0 && fib_arr[1] == 1){
+      return [i, sum];
+    }
+  }
+}
+
+int fib_sum(int n){
+print(n);
+  List<int> period = pisanno(10);
+  //print(period);
+  int m = n~/period[0];
+  int sum = period[1] * m;
+   
+  n = n%period[0];
+  //print(n);
+  if (n != 0){
+    sum += 1;
+  }
+  List<int> fib_arr = [0,1];
+  for(int i = 2; i<=n; i++){
+    int value = (fib_arr[0] + fib_arr[1]) % 10;
+    fib_arr[0] = fib_arr[1];
+    fib_arr[1] = value;
+    sum += value * value;
+    }
+//print(fib_arr);
+//print(sum);
+return sum % 10 ;
+}
+
+void main(){
+  num answer = fib_sum(num.parse(stdin.readLineSync()));
+  print(answer);
+}
