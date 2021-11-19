d89b8524461be820bb54ea7e64c22e9956c7292a
ChewxDev <91054021+ChewxDev@users.noreply.github.com>
2021-11-19 03:00:07 +0100

    Update 7_fibonacci_number_again.dart

--- 7_fibonacci_number_again.dart
+++ 7_fibonacci_number_again.dart
@@ -8,7 +8,7 @@ int fib(int n, int m){
 
   List fib_arr = [0, 1];
   for(int i = 2; i<=n; i++){
-    int value = (fib_arr[0] + fib_arr[1]) % m;
+    int value = (fib_arr[0] + fib_arr[1]) % m;//Allows the user select the modulo and ultimately the pisanno period themselves 
     fib_arr[0] = fib_arr[1];
     fib_arr[1] = value;
   }
